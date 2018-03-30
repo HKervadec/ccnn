bcc03d3ada27a21e220bb2bd05a51703d119d7d6
Hoel Kervadec <hoel.kervadec.1@etsmtl.net>
2018-03-30 00:16:52 -0400

    Fix demo paths to run from root

--- src/demo.py
+++ src/demo.py
@@ -13,7 +13,7 @@ import caffe
 
 # Function to get VOC color map. Don't change the image being loaded here.
 def palette_gt(gt):
-    palette_im = Image.open('../models/examples/gt1.png')
+    palette_im = Image.open('./models/examples/gt1.png')
     gt.putpalette(palette_im.palette)
     return gt
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     net = caffe.Net(net_def, weights, caffe.TEST)
 
     # Load Image
-    im = Image.open('../models/examples/im2.jpg')
+    im = Image.open('./models/examples/im2.jpg')
     im = np.array(im, dtype=np.float32)
     im = im[:, :, ::-1]               # Change to BGR
     mean = np.array((104.00698793, 116.66876762, 122.67891434))
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     net.forward()
     out = Image.fromarray(net.blobs['upscore-crf'].data[0, 0].astype(np.uint8), mode='P')
     out = palette_gt(out)
-    out.save('../models/examples/result.png')
+    out.save('./models/examples/result.png')
 
     # Classes Predicted
     print('Classes Predicted:', np.unique(net.blobs['upscore-crf'].data[0, 0].astype(np.uint8)))

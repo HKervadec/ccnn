aae48e4f4ab82b393f0bd22b2cfabe7c64495fb8
Hoel Kervadec <hoel.kervadec.1@etsmtl.net>
2018-03-30 00:45:42 -0400

    Cleanup

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3.6
+
 # --------------------------------------------------------
-# CCNN 
+# CCNN
 # Copyright (c) 2015 [See LICENSE file for details]
 # Written by Deepak Pathak, Philipp Krahenbuhl
 # --------------------------------------------------------
@@ -7,26 +9,29 @@
 import numpy as np
 
 VOC_DIR = '/mnt/a/pathak/fcn_mil_cache/VOC2012'
-CHANNEL_MEAN = np.array([104.00698793,116.66876762,122.67891434])
+CHANNEL_MEAN = np.array([104.00698793, 116.66876762, 122.67891434])
+
 
 def idsVOC(type='train'):
-	if type == 'train':
-		return [l.strip() for l in open(VOC_DIR+'/train.txt','r')]
-	if type == 'trainval':
-		return [l.strip() for l in open(VOC_DIR+'/trainval.txt','r')]
-	if type == 'trainval':
-		return [l.strip() for l in open(VOC_DIR+'/test.txt','r')]
-	return [l.strip() for l in open(VOC_DIR+'/val.txt','r')]
+    if type == 'train':
+        return [l.strip() for l in open(VOC_DIR+'/train.txt', 'r')]
+    if type == 'trainval':
+        return [l.strip() for l in open(VOC_DIR+'/trainval.txt', 'r')]
+    if type == 'trainval':
+        return [l.strip() for l in open(VOC_DIR+'/test.txt', 'r')]
+    return [l.strip() for l in open(VOC_DIR+'/val.txt', 'r')]
 
-t0,t1 = 0,0
-def fetchVOC( id ):
-	from skimage import io
-	from time import time
-	global t0,t1
-	t0 += time()
-	im = io.imread(VOC_DIR+"/JPEGImages/%s.jpg"%id)
-	tim = im[:,:,::-1].transpose((2,0,1))-CHANNEL_MEAN[:,None,None]
-	lbl = io.imread(VOC_DIR+"/SegmentationClassGray/%s.png"%id)
-	t1 += time()
-	return tim[None],lbl[None,None]
 
+t0, t1 = 0, 0
+
+
+def fetchVOC(id):
+    from skimage import io
+    from time import time
+    global t0, t1
+    t0 += time()
+    im = io.imread(VOC_DIR+"/JPEGImages/%s.jpg" % id)
+    tim = im[:, :, ::-1].transpose((2, 0, 1))-CHANNEL_MEAN[:, None, None]
+    lbl = io.imread(VOC_DIR+"/SegmentationClassGray/%s.png" % id)
+    t1 += time()
+    return tim[None], lbl[None, None]
--- src/train.py
+++ src/train.py
@@ -1,69 +1,76 @@
+#!/usr/bin/env python3.6
+
 # --------------------------------------------------------
-# CCNN 
+# CCNN
 # Copyright (c) 2015 [See LICENSE file for details]
 # Written by Deepak Pathak, Philipp Krahenbuhl
 # --------------------------------------------------------
 
-from config import *
-import ccnn
-import python_layers, dataset
 from glob import glob
-import numpy as np
 from time import time
 from sys import argv
 
-caffe.set_mode_gpu()
-caffe.set_device(0)
+import numpy as np
 
-MODEL_PROTOTXT = '../models/fcn_8s/train_8s.prototxt'
-MODEL_INIT = '../models/imagenet_pretrained_models/vgg_init_8s.caffemodel'
+import caffe
+import ccnn
+import python_layers
+import dataset
 
-# MODEL_PROTOTXT = '../models/fcn_32s/train_32s.prototxt'
-# MODEL_INIT = '../models/imagenet_pretrained_models/vgg_init_32s.caffemodel'
 
-MODEL_SAVE = '../models/ccnn_models/ccnn.caffemodel'
+if __name__ == "__main__":
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
 
-if len(argv)>1:
-	MODEL_SAVE = argv[1]
-doTest = False
+    MODEL_PROTOTXT = './models/fcn_8s/train_8s.prototxt'
+    MODEL_INIT = './models/imagenet_pretrained_models/vgg_init_8s.caffemodel'
 
-SOLVER_STR = """train_net: "{TRAIN_NET}"
-base_lr: 1e-6
-lr_policy: "step"
-gamma: 0.1
-stepsize: 40000
-display: 20
-max_iter: 35000
-momentum: 0.99
-weight_decay: 0.0000005
-#average_loss: 1
-"""
+    # MODEL_PROTOTXT = '../models/fcn_32s/train_32s.prototxt'
+    # MODEL_INIT = '../models/imagenet_pretrained_models/vgg_init_32s.caffemodel'
 
-SOLVER_STR = SOLVER_STR.replace( "{TRAIN_NET}", MODEL_PROTOTXT )
+    MODEL_SAVE = './models/ccnn_models/ccnn.caffemodel'
 
-t0 = time()
-solver = caffe.get_solver_from_string(SOLVER_STR)
-solver.net.copy_from(MODEL_INIT) # Note that this does not copy the interpolation params!
-print "Load model %fs"%(time()-t0)
+    if len(argv) > 1:
+        MODEL_SAVE = argv[1]
+    doTest = False
 
-for it in range(35):
-	t0 = time()
-	solver.step(1000)
-	t1 = time()
-	print "%4d iterations t ="%((it+1)*1000), t1-t0
-	solver.net.save(MODEL_SAVE)
-	if (it+1)%5==0 and it>10:
-		solver.net.save(MODEL_SAVE + '_'+str(it+1))
-	if doTest and it>30:
-		import subprocess
-		try:
-			sp.wait()
-		except:
-			pass
-		sp = subprocess.Popen(['python', 'test_argmax.py'])
+    SOLVER_STR = """train_net: "{TRAIN_NET}"
+    base_lr: 1e-6
+    lr_policy: "step"
+    gamma: 0.1
+    stepsize: 40000
+    display: 20
+    max_iter: 35000
+    momentum: 0.99
+    weight_decay: 0.0000005
+    #average_loss: 1
+    """
 
-if doTest:
-	try:
-		sp.wait()
-	except:
-		pass
+    SOLVER_STR = SOLVER_STR.replace("{TRAIN_NET}", MODEL_PROTOTXT)
+
+    t0 = time()
+    solver = caffe.get_solver_from_string(SOLVER_STR)
+    solver.net.copy_from(MODEL_INIT)  # Note that this does not copy the interpolation params!
+    print("Load model %fs" % (time()-t0))
+
+    for it in range(35):
+        t0 = time()
+        solver.step(1000)
+        t1 = time()
+        print("%4d iterations t =" % ((it+1)*1000), t1-t0)
+        solver.net.save(MODEL_SAVE)
+        if ((it+1) % 5) == 0 and it > 10:
+            solver.net.save(MODEL_SAVE + '_'+str(it+1))
+        if doTest and it > 30:
+            import subprocess
+            sp = subprocess.Popen(['python', 'test_argmax.py'])
+            try:
+                sp.wait()
+            except:
+                pass
+
+    if doTest:
+        try:
+            sp.wait()
+        except:
+            pass
